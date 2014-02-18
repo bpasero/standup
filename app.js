33b12fa5aec8405cb403db01989d21e600d6ec9c
user <user@domain>
2014-02-18 08:26:21 +0000

    get rid of jade

--- app.js
+++ app.js
@@ -4,17 +4,16 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
+
+var routes = require('./routes');
+var user = require('./routes/user');
 
 var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
