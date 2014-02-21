152ce455d980fc29a61333b7dd141e88427ba799
user <user@domain>
2014-02-21 10:45:41 +0000

    redmond

--- config.js
+++ config.js
@@ -4,14 +4,10 @@
 // To change the list of users, simply comment out or add users and F5 https://stand.azurewebsites.net/
 
 module.exports.users = [
-	{ "name": "Martin" },
-	{ "name": "Isidor" },
-	{ "name": "Joh" },
-	{ "name": "André" },
-	{ "name": "João" },
-	{ "name": "Ben" },
-	{ "name": "Alex" },
-	{ "name": "Erich" },
-	{ "name": "Dirk" },
-	{ "name": "Redmond" }
+	{ "name": "Sofiaaan" },
+	{ "name": "ChrisD" },
+	{ "name": "Jeff" },
+	{ "name": "Steven" },
+	{ "name": "Chris5" },
+	{ "name": "Zurich" },
 ];
