13788d3ca830b2301b7775f01368b8196377a6a7
user <user@domain>
2014-03-10 16:32:52 +0000

    show image label

--- server/routes/image.js
+++ server/routes/image.js
@@ -24,10 +24,12 @@ exports.route = function(req, res) {
       try {
         var data = JSON.parse(str);
         var url = data.images[0].url;
+        var label = data.images[0].copyright;
         
         res.writeHead(200, {"Content-Type": "application/json"});
         res.end(JSON.stringify({
-          url: 'http://www.bing.com' + url
+          url: 'http://www.bing.com' + url,
+          label: label
         }));
       } catch (error) {
         res.writeHead(200, {"Content-Type": "application/json"});
