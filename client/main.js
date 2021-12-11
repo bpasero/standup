3e7973740e2f287db87c563510a3c2ef7622347a
Benjamin Pasero <benjamin.pasero@microsoft.com>
2021-12-11 13:12:01 +0100

    copyrights

--- client/main.js
+++ client/main.js
@@ -1,4 +1,9 @@
-/// <reference path="./declare/jquery.d.ts" />
+/*---------------------------------------------------------------------------------------------
+ *  Copyright (c) Microsoft Corporation. All rights reserved.
+ *  Licensed under the MIT License. See License.txt in the project root for license information.
+ *--------------------------------------------------------------------------------------------*/
+
+'use strict';
 
 define([
 	'socketio'
